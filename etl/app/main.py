973e0e673955e6b624bbbfa31b47d824652acd8a
aleksey <football4113@yandex.ru>
2024-11-05 18:26:34 +0300

    Add etl

---
+++ etl/app/main.py
@@ -0,0 +1,93 @@
+import json
+
+import clickhouse_connect
+from pydantic import ValidationError, BaseModel
+from concurrent.futures import ThreadPoolExecutor
+
+from config import etl_settings, KafkaTopics
+from logger import logger
+import backoff
+
+from kafka import KafkaConsumer
+from schemas.base import (
+    PageTimeSpend,
+    QualityChangeEvent,
+    SearchFilterEvent,
+    UserPageClick,
+    VideoCompletedEvent,
+)
+
+logger.info(f"kafka_bootstrap: {etl_settings.kafka_bootsrap}")
+
+KAFKA_BROKERS = etl_settings.kafka_bootsrap
+
+CLICKHOUSE_HOST = etl_settings.ch_host
+CLICKHOUSE_PORT = etl_settings.ch_port
+CLICKHOUSE_DATABASE = etl_settings.ch_database
+CLICKHOUSE_USER = etl_settings.ch_user
+CLICKHOUSE_PASSWORD = etl_settings.ch_password
+
+poll_timeout = 1000  # in milliseconds
+batch_size = 1000
+
+
+clickhouse_client = clickhouse_connect.get_client(
+    host=CLICKHOUSE_HOST,
+    port=CLICKHOUSE_PORT,
+    database=CLICKHOUSE_DATABASE,
+    user=CLICKHOUSE_USER,
+    password=CLICKHOUSE_PASSWORD,
+)
+
+
+def insert_data_to_clickhouse(name_table, data: list):
+    clickhouse_client.execute(
+        f"INSERT INTO {name_table} FORMAT JSONEachRow", data
+    )
+    logger.info(f"Inserted {len(data)} rows to clickhouse")
+
+
+@backoff.on_exception(
+    backoff.expo,
+    Exception,
+    max_tries=3,
+)
+def consume_messages(topic: str, model: BaseModel):
+    consumer = KafkaConsumer(
+        topic,
+        bootstrap_servers=KAFKA_BROKERS,
+        auto_offset_reset="earliest",
+        group_id=f"group_{topic}",
+        value_deserializer=lambda m: json.loads(m.decode("utf-8")),
+    )
+    logger.info(f"Started consumer for topic: {topic}")
+
+    while True:
+        messages = consumer.poll(
+            timeout_ms=poll_timeout, max_records=batch_size
+        )
+
+        for tp, msgs in messages.items():
+            batch = []
+            for message in msgs:
+                try:
+                    validated_data = model(**message.value)
+                    batch.append(validated_data)
+                except ValidationError as e:
+                    logger.info(f"Validation error in topic {topic}: {e}")
+            if batch:
+                insert_data_to_clickhouse(batch)
+
+
+if __name__ == "__main__":
+    topics = {
+        KafkaTopics.PAGE_TIME_SPEND.value: PageTimeSpend,
+        KafkaTopics.QUALITY_CHANGE.value: QualityChangeEvent,
+        KafkaTopics.SEARCH_FILTER.value: SearchFilterEvent,
+        KafkaTopics.USER_PAGE_CLICK.value: UserPageClick,
+        KafkaTopics.VIDEO_COMPLETED.value: VideoCompletedEvent,
+    }
+
+    with ThreadPoolExecutor(max_workers=len(topics)) as executor:
+        for topic, model in topics.items():
+            executor.submit(consume_messages, topic, model)
---
+++ etl/app/schemas/base.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+import orjson
+from pydantic import UUID4, BaseModel, Field
+
+
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+
+class OrjsonBaseModel(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class QualityChangeEvent(OrjsonBaseModel):
+    event_type: str = "quality_change"
+    user_id: UUID4 = Field(default=None)
+    video_id: UUID4
+    old_quality: str
+    new_quality: str
+    timestamp: datetime
+
+
+class VideoCompletedEvent(OrjsonBaseModel):
+    event_type: str = "video_completed"
+    user_id: UUID4 = Field(default=None)
+    video_id: UUID4
+    timestamp: datetime
+
+
+class SearchFilterEvent(OrjsonBaseModel):
+    event_type: str = "search_filter"
+    user_id: UUID4 = Field(default=None)
+    filters: dict  # Словарь с фильтрами, например {"genre": "action", "rating": "9"}
+    timestamp: datetime
+
+
+class PageTimeSpend(OrjsonBaseModel):
+    event_type: str = "page_time_spend"
+    user_id: UUID4 = Field(default=None)
+    page_name: str
+    entry_time: datetime
+    exit_time: datetime = Field(default=None)
+
+
+class UserPageClick(OrjsonBaseModel):
+    event_type: str = "user_page_click"
+    user_id: UUID4 = Field(default=None)
+    session_id: str = Field(default=None)
+    timestamp: datetime
+    page_name: str
+    element_id: int
+    element_type: str
